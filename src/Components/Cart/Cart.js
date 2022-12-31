9bfd460dbe47cacb0831ad745964767f2cf0adbf
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2022-12-31 11:33:40 +0200

    first commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux";
-import { getCartItems } from "../../redux/cartSlice";
+import { getCartItems, getTotalPrice } from "../../redux/cartSlice";
 import CartItem from "./CartItem";
 
 const Cart = () => {
   const cartItems = useSelector(getCartItems);
+  const totalPrice = useSelector(getTotalPrice);
   return (
     <div>
       <img
@@ -11,10 +12,10 @@ const Cart = () => {
         src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png"
         alt="icon"
       />
+      <h3>TOTAL: $ {totalPrice}</h3>
       {cartItems.map((cartItem) => (
         <CartItem cartItem={cartItem} />
       ))}
-      <CartItem />
     </div>
   );
 };
